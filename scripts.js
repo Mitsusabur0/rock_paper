376d86cd2c9a9eabe2ab46a72cfbb5921f6cd75c
mitsusaburo <bschindlerm@gmail.com>
2023-10-04 16:04:51 -0300

    Create the game with UI

--- scripts.js
+++ scripts.js
@@ -4,26 +4,64 @@ function getComputerChoice() {
     console.log(choice)
 }
 
-function playGame(playerSelection, computerSelection) {
+
+let buttons = document.querySelector("#buttons");
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case "rock":
+            playGame("rock");
+            break;
+        case "paper":
+            playGame("paper");
+            break;
+        case "scissors":
+            playGame("scissors");
+            break;
+    }
+})
+
+
+let selections = document.querySelector("#selections");
+let result = document.querySelector("#result");
+let currentPlayerScore = document.querySelector("#playerScore");
+let currentComputerScore = document.querySelector("#computerScore");
+let finalResult = document.querySelector("#finalResult");
+playerCounter = 0;
+computerCounter = 0;
+
+function playGame(playerSelection) {
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    selections.innerText = `you: ${playerSelection.toUpperCase()} vs computer: ${computerSelection.toUpperCase() }`;
+
     if (playerSelection === computerSelection) {
-        console.log("No one wins");
+        result.innerText = "No one wins";
+
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper"))  {
-        console.log("Player wins!");
+        result.innerText = "Player wins!";
+        playerCounter++;
+        currentPlayerScore.innerText = `Player score: ${playerCounter}`;
+
     } else if ((computerSelection === "rock" && playerSelection === "scissors") ||
         (computerSelection === "paper" && playerSelection === "rock") ||
         (computerSelection === "scissors" && playerSelection === "paper"))  {
-        console.log("Computer wins!");
+        result.innerText =  "Computer wins!";
+        computerCounter++;
+        currentComputerScore.innerText = `Computer score: ${computerCounter}`;
+
+
     } else {
-        console.log("Invalid input");
+        result.innerText =  "no winners here";
+    }
+
+    if (playerCounter === 5) {
+        finalResult.innerText = "PLAYER WINSSSSSSS THE GAMEEEEEEEEEEE";
+    } else if (computerCounter === 5) {
+        finalResult.innerText = "COMPUTER WINSSSSSS THE GAMEEEEEEEEEE";
     }
 }
 
-for (let  i = 0; i < 5; i++) {
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    let playerChoice = prompt("Rock, paper, scissors? ").toLowerCase();
-    console.log(`Player: ${playerChoice} - Computer: ${computerChoice}`);
-    playGame(playerChoice, computerChoice);
-}
     
