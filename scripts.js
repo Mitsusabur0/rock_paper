694bac5a777de9915a46749c41ad2810b7a72739
mitsusaburo <bschindlerm@gmail.com>
2023-10-04 15:09:01 -0300

    commiting past changes

--- scripts.js
+++ scripts.js
@@ -4,11 +4,26 @@ function getComputerChoice() {
     console.log(choice)
 }
 
-let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-console.log(computerChoice);
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("No one wins");
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper"))  {
+        console.log("Player wins!");
+    } else if ((computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper"))  {
+        console.log("Computer wins!");
+    } else {
+        console.log("Invalid input");
+    }
+}
 
-
-
-
-
-
+for (let  i = 0; i < 5; i++) {
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let playerChoice = prompt("Rock, paper, scissors? ").toLowerCase();
+    console.log(`Player: ${playerChoice} - Computer: ${computerChoice}`);
+    playGame(playerChoice, computerChoice);
+}
+    
